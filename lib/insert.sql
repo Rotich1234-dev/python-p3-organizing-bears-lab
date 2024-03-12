d5c9270070a6412a9439af6220529c7477e61b86
Rotich <jane.rotich@student.moringaschool.com>
2024-03-12 16:45:09 +0300

    feat:finish test

---
+++ lib/insert.sql
@@ -0,0 +1,11 @@
+-- Inserting bears into the bears table
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Mr. Chocolate', 5, 'M', 'Brown', 'Friendly', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Rowdy', 3, 'M', 'Black', 'Playful', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Tabitha', 2, 'F', 'Gray', 'Calm', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Sergeant Brown', 7, 'M', 'Brown', 'Disciplined', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Melissa', 4, 'F', 'White', 'Sweet', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Grinch', 6, 'M', 'Green', 'Mischievous', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Wendy', 8, 'F', 'Brown', 'Laid-back', 1);
+
+-- Inserting an unnamed bear
+INSERT INTO bears (age, sex, color, temperament, alive) VALUES (1, 'F', 'Unknown', 'Mysterious', 1);
